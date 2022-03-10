61fa5d49eac2e9f7e9cbfb9c08437148439ac476
Sharonchep <sharon30@gmail.com>
2022-03-10 18:03:10 +0300

    add new changes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function date(){
+  let day=document.getElementById(day); 
+  let month=document.getElementById(month); 
+  let year=document.getElementById(year);
+  let date=day +"/"+ month+"/"+year
+  console.log (day);
+  let males=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  let females=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];
+  let days={sunday,monday,tuesday,wednesday,thursday,friday,saturday}
+}
+
